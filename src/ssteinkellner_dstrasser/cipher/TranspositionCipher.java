34a5da367e3d00a24fa6e34a320d1a1373fcfb5d
DominikStrasser3CHIT <dstrasser@student.tgm.ac.at>
2014-04-27 13:22:47 +0200

    SEW Strasser

--- src/ssteinkellner_dstrasser/cipher/TranspositionCipher.java
+++ src/ssteinkellner_dstrasser/cipher/TranspositionCipher.java
@@ -1,4 +1,4 @@
-package ssteinkellner_dstrasser.cipher;
+
 
 /**
  * 
@@ -8,26 +8,96 @@ package ssteinkellner_dstrasser.cipher;
  */
 public class TranspositionCipher implements Cipher
 {
-   public TranspositionCipher(int transpositionLevel)
-   {
-       
-   }
-   public void setTranspositionLevel(int level)
-   {
-       
-   }
-   /**
-     * Verschlüsselt den eingelesenen Text und gibt ihn verschlüsselt zurück
-     */
-   public String encrypt (String text)
-   {
-        return text;
-   }
-   /**
-    * Entschlüsselt den eingelesenen Text und gibt ihn entschlüsselt zurück
-    */
-   public String decrypt (String text)
-   {
-        return text;
-   }
+	private int transpositionLevel;
+	
+
+	public TranspositionCipher(int transpositionLevel) throws SecretException
+	{
+		setTranspositionLevel(transpositionLevel);
+	}
+	
+	
+	public void setTranspositionLevel(int level) throws SecretException
+	{
+		if(level<0){
+			throw(new SecretException("transpositionLevel hast to be positive"));
+		}
+		
+		transpositionLevel = level;
+	}
+	
+	/**
+	 * VerschlÃÂ¼sselt den eingelesenen Text und gibt ihn verschlÃÂ¼sselt zurÃÂ¼ck
+	 */
+	public String encrypt (String text)
+	{
+		String[] teile = new String[transpositionLevel];
+		// jeder string im array ist eine transpositionsstufe
+		
+		// array initialisieren
+		for(int i=0;i<teile.length;i++){
+			teile[i]="";
+		}
+		
+		// in arrays aufteilen
+		int stufe=0;
+		for(int i=0;i<text.length();i++){
+//			System.out.println(stufe + " : "+ teile[stufe] + " + " + text.charAt(i));	// debugausgabe
+			teile[stufe] += text.charAt(i);
+			
+			stufe++;
+			if(stufe==teile.length){
+				stufe=0;
+			}
+		}
+		
+		text = "";
+		
+		// array zu einem text zusammenfassen
+		for(int i=0;i<teile.length;i++){
+			if(teile[i].length()<teile[0].length()){
+				teile[i] += (char) 191;
+			}	//unicode zeichen zum auffÃ¼llen
+			
+			text += teile[i];
+		}
+		
+		return text;
+	}
+	
+	/**
+	 * EntschlÃÂ¼sselt den eingelesenen Text und gibt ihn entschlÃÂ¼sselt zurÃÂ¼ck
+	 */
+	public String decrypt (String text)
+	{
+		String[] teile = new String[transpositionLevel];
+		// jeder string im array ist eine transpositionsstufe
+		
+		int length = (int) Math.ceil((double) text.length()/(double)transpositionLevel);
+		// berechung, wie lang jede zeile ist (mit aufrunden)
+//		System.out.println(text.length() + "/" + transpositionLevel + " = " + length);	//debugausgabe
+		
+		if(teile.length*length != text.length()){
+			return "ERROR: text and transpositionlevel are incompatible!";
+		}
+		
+		// aufteilung des textes in stufen
+		int pos;
+		for(int i=0;i<teile.length;i++){
+			pos = i*length;
+			teile[i] = text.substring(pos, pos+length);
+			System.out.println(teile[i]);	//debugausgabe
+		}
+		
+		text = "";
+		
+		// zerlegen der stufen in text
+		for(int i=0;i<teile[0].length();i++){
+			for(int stufe=0;stufe<teile.length;stufe++){
+				text += teile[stufe].charAt(i);
+			}
+		}
+		
+		return text;
+	}
 }
